614ae564385538ad7500f500d1427821b494de6b
wilsonmike <michaelwilsonsj@hotmail.com>
2022-04-25 12:30:53 -0400

    clean data && scatterplot (negative correlation)

---
+++ script.js
@@ -0,0 +1,31 @@
+async function getData() {
+    const carsDataResponse = await fetch('https://storage.googleapis.com/tfjs-tutorials/carsData.json');
+    const carsData = await carsDataResponse.json();
+    const cleaned = carsData.map(car => ({
+        mpg: car.Miles_per_Gallon,
+        horsepower: car.Horsepower,
+    }))
+    .filter(car => (car.mpg != null && car.horsepower != null));
+
+    return cleaned;
+}
+
+async function run() {
+    const data = await getData();
+    const values = data.map(d => ({
+        x: d.horsepower,
+        y: d.mpg,
+    }));
+
+    tfvis.render.scatterplot(
+        {name: 'HP v MPG'},
+        {values},
+        {
+            xLabel: 'HP',
+            yLabel: 'MPG',
+            height: 300
+        }
+    )
+}
+
+document.addEventListener('DOMContentLoaded', run);
